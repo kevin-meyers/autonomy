59c176ef2655e1a9586a93a0481dbdffdbf41668
Kevin <kevinm1776@gmail.com>
2020-02-05 11:46:44 -0800

    Created new app and added to settings.

--- autonomy/autonomy/settings.py
+++ autonomy/autonomy/settings.py
@@ -31,14 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'task',
-    'accounts',
+>>>>>>> 195514f... Created new app and added to settings.
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'task',
+    'project',
 ]
 
 MIDDLEWARE = [
