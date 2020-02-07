152505622fc067d7e7f261580e71d465d96aca5d
David Evans <evansdav.a@gmail.com>
2020-02-07 15:21:00 -0800

    Added user creation and sign in

--- autonomy/autonomy/settings.py
+++ autonomy/autonomy/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'task',
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -100,6 +101,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
