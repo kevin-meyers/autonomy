7ea4d256e9c95d381c469b8925264bddbd8b0654
David Evans <evansdav.a@gmail.com>
2020-02-08 18:47:34 -0800

    Changed url structure and removed leftover bootstrap from old code

--- autonomy/autonomy/urls.py
+++ autonomy/autonomy/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    # Accounts app
-    path('signup/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', include('task.urls')),
 ]
