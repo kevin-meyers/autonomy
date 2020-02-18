c79ca3d1b14dfa9bcc08924bb04178194fc0ab25
Kevin <kevinm1776@gmail.com>
2020-02-17 19:30:51 -0800

    Enable local host.

--- autonomy/autonomy/settings.py
+++ autonomy/autonomy/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['angular-lambda-268517.appspot.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'angular-lambda-268517.appspot.com']
 
 
 # Application definition
